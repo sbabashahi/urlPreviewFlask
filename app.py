222ee01742982404ec7e44c5a3b03fae6d913c6a
saeed <sbabashahi@gmail.com>
2019-06-16 21:04:13 +0430

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def link_preview():
+    url = request.args.get('url')
+    if url:
+        if not url.startswith('http'):
+            url = 'http://' + url
+
+        resp = requests.get(url)
+
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content)
+            data = {
+                'title': soup.title.text if soup.title else '',
+            }
+            return SuccessResponse(data).send()
+        else:
+            return ErrorResponse(error='Error in getting resource.').send()
+    else:
+        return ErrorResponse(error='No url detected').send()
+
+
+class SuccessResponse:
+    def __init__(self, data=None, message='', status=200):
+        if data is None:
+            data = {}
+        self.data = data
+        self.message = message
+        self.status = status
+
+    def send(self):
+        return jsonify(self.__dict__)
+
+
+class ErrorResponse:
+    def __init__(self, error='', message='', status=400):
+        self.error = error
+        self.message = message
+        self.status = status
+
+    def send(self):
+        return jsonify(self.__dict__)
+
+
+if __name__ == '__main__':
+    app.run()
